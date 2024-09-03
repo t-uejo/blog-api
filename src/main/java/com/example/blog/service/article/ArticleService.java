75b40e28fb2d4232d4a1e16b86814b6257dcf84b
t-uejo <tiki.ur.tiki@icloud.com>
2024-09-04 08:28:10 +0900

    refactor：NULLを返さないようOptionalを使う

--- src/main/java/com/example/blog/service/article/ArticleService.java
+++ src/main/java/com/example/blog/service/article/ArticleService.java
@@ -1,15 +1,18 @@
 package com.example.blog.service.article;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class ArticleService {
-    public ArticleEntity findById(long id){
-        return new ArticleEntity(
-                id,
-                "title",
-                "content",
-                LocalDateTime.now(),
-                LocalDateTime.now()
+    public Optional<ArticleEntity> findById(long id){
+        return Optional.of(
+            new ArticleEntity(
+                    id,
+                    "title",
+                    "content",
+                    LocalDateTime.now(),
+                    LocalDateTime.now()
+            )
         );
     }
 }
