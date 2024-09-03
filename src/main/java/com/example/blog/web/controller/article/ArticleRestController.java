7f01e00321f455305c863c11bf49b3e5542a3b64
t-uejo <tiki.ur.tiki@icloud.com>
2024-09-03 21:25:07 +0900

    feature：GET /articles/{id} を追加

---
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -0,0 +1,13 @@
+package com.example.blog.web.controller.article;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleRestController {
+    @GetMapping("/articles/{id}")
+    public String showArticle(@PathVariable("id") long id){
+        return "id = " + id;
+    }
+}
