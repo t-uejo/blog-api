0c26006a82c6c1186dfbfef3966b2f92fa642b4f
t-uejo <tiki.ur.tiki@icloud.com>
2024-09-04 22:05:17 +0900

    feature：Lombokを導入

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,21 +1,18 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import java.time.LocalDateTime;
 
 @RestController
+@RequiredArgsConstructor
 public class ArticleRestController {
 
     private final ArticleService articleService;
-
-    public ArticleRestController(ArticleService articleService){
-        this.articleService = articleService;
-    }
 
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id){
