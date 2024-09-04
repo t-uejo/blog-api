6ce11d25235009b177f917aad68517e9cf18ea14
t-uejo <tiki.ur.tiki@icloud.com>
2024-09-04 18:18:14 +0900

    feature：articleServiceをDIするよう修正

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.time.LocalDateTime;
 
 @RestController
 public class ArticleRestController {
-    private final ArticleService articleService = new ArticleService();
+
+    private final ArticleService articleService;
+
+    public ArticleRestController(ArticleService articleService){
+        this.articleService = articleService;
+    }
 
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id){
