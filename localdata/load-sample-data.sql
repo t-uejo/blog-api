17319c58b274f627bb45a75be76c4283573ccbc3
t-uejo <tiki.ur.tiki@icloud.com>
2024-09-06 19:03:32 +0900

    feature：初期データ投入用スクリプトを作成

---
+++ localdata/load-sample-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO articles (title, body) VALUES ('タイトル1です。', '本文1です。');
+INSERT INTO articles (title, body) VALUES ('タイトル2です。', '本文2です。');
+INSERT INTO articles (title, body) VALUES ('タイトル3です。', '本文3です。');
