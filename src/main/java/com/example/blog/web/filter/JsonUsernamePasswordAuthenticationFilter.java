02f6b4104463bb5940b81d405c6e90a8602dab17
t-uejo <tiki.ur.tiki@icloud.com>
2024-10-16 19:32:12 +0900

    feature：SecurityContextRepositoryに認証情報が保存される設定を追加、認証が成功/失敗した場合のレスポンスを定義

--- src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -2,14 +2,34 @@ package com.example.blog.web.filter;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+import java.util.List;
 
 public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    public JsonUsernamePasswordAuthenticationFilter(SecurityContextRepository setSecurityContextRepository){
+        super();
+        setSecurityContextRepository(setSecurityContextRepository);
+        setAuthenticationSuccessHandler((req, res, auth) ->{
+            res.setStatus(HttpServletResponse.SC_OK);
+        });
+        setAuthenticationFailureHandler((req, res, auth) ->{
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        });
+    }
+
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+        return UsernamePasswordAuthenticationToken.authenticated(
+                "dummy-user",
+                "dummy-pass",
+                List.of());
     }
 }
