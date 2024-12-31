1b9117d23bda12053aa587beca9b2b15971d456e
t-uejo <info@taikiuejo.com>
2024-12-31 16:49:18 +0900

    fix：JaksonがJava8のLocalDateTimeを認識できるように設定

--- src/main/java/com/example/blog/config/ObjectMapperConfig.java
+++ src/main/java/com/example/blog/config/ObjectMapperConfig.java
@@ -1,13 +1,27 @@
 package com.example.blog.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class ObjectMapperConfig {
+
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        var timeModule = new JavaTimeModule();
+        timeModule.addSerializer(
+                LocalDateTime.class,
+                new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME)
+        );
+
+        return new ObjectMapper().registerModule(timeModule);
     }
+
 }
